9e7dba6c60d01449aeec02cc0204132142bd4389
Khushi Porwal <khushiporwal98@gmail.com>
2026-07-15 23:04:23 +0530

    Add cart , address

--- Backend/routes/order/order.js
+++ Backend/routes/order/order.js
@@ -1,11 +1,56 @@
-const express = require("express")
-const router = express.Router();
-const authMiddleware = require("../../middleware/authMiddleware")
-const {placeOrder, getMyOrder, getSingleOrder, updateOrderStatus, cancelOrder} = require("../../controller/order/PlaceOrder")
+const express = require("express");
 
-router.post("/create-order", authMiddleware, placeOrder)
-router.get("/get-my-order", authMiddleware, getMyOrder)
-router.get("/get-single-order/:orderId",getSingleOrder)
-router.put("/update-order/:orderId/status", updateOrderStatus)
-router.delete("/cancel-order/:orderId",cancelOrder)
-module.exports = router
+const router = express.Router();
+
+const authMiddleware = require(
+    "../../middleware/authMiddleware"
+);
+
+const adminMiddleware = require(
+    "../../middleware/adminMiddleware"
+);
+
+const {
+    placeOrder,
+    getMyOrder,
+    getSingleOrder,
+    updateOrderStatus,
+    cancelOrder,
+} = require(
+    "../../controller/order/PlaceOrder"
+);
+
+
+router.post(
+    "/place-order",
+    authMiddleware,
+    placeOrder
+);
+
+router.get(
+    "/get-my-order",
+    authMiddleware,
+    getMyOrder
+);
+
+router.get(
+    "/get-single-order/:orderId",
+    authMiddleware,
+    getSingleOrder
+);
+
+router.put(
+    "/update-order/:orderId/status",
+    authMiddleware,
+    adminMiddleware,
+    updateOrderStatus
+);
+
+router.delete(
+    "/cancel-order/:orderId",
+    authMiddleware,
+    cancelOrder
+);
+
+
+module.exports = router;
